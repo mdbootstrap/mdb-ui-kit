005bfeb7e669edec2a601c19fdd9067ef14c6dcd
FezVrasta <fez@tekhy.net>
2014-09-07 13:49:49 +0200

    .box > .check

--- scripts/material.js
+++ scripts/material.js
@@ -5,7 +5,7 @@ $(function (){
     ripples.init(".btn:not(.btn-link), .navbar a, .nav-tabs a, .withripple");
 
     // Add fake-checkbox to material checkboxes
-    $(".checkbox label input").after("<span class=ripple></span><span class=check></span><span class=box></span>");
+    $(".checkbox label input").after("<span class=ripple></span><span class=check></span>");
 
     // Add fake-radio to material radios
     $(".radio label input").after("<span class=ripple></span><span class=circle></span><span class=check></span>");
@@ -23,19 +23,6 @@ $(function (){
             $(this).addClass("empty");
         }
     });
-
-    // Material inputs engine (ripple effect)
-    /*$(document).on("mouseup", ".checkbox label, .radio label", function() {
-        console.log("click");
-        var $ripple = $(this).find(".ripple");
-
-        $ripple.addClass("animate");
-        var timer = setTimeout(function() {
-            console.log("out");
-            clearTimeout(timer);
-            $ripple.removeClass("animate");
-        }, 800);
-    });*/
 
     $(document).on("change", ".form-control", function() {
         if ($(this).val() !== "") {
