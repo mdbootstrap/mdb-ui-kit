86714295771af2934040239ca96daf7f3fdbb673
Federico Zivolo <zivolof@p2000.it>
2014-09-18 10:43:28 +0200

    fileinput support

--- material/material.js
+++ material/material.js
@@ -5,10 +5,10 @@ $(function (){
     ripples.init(".btn:not(.btn-link), .navbar a, .nav-tabs a, .withripple");
 
     // Add fake-checkbox to material checkboxes
-    $(".checkbox label input").after("<span class=ripple></span><span class=check></span>");
+    $(".checkbox > label > input").after("<span class=check></span>");
 
     // Add fake-radio to material radios
-    $(".radio label input").after("<span class=ripple></span><span class=circle></span><span class=check></span>");
+    $(".radio > label > input").after("<span class=ripple></span><span class=circle></span><span class=check></span>");
 
     // Add elements for material inputs
     $("input.form-control, textarea.form-control, select.form-control").each( function() {
@@ -22,6 +22,12 @@ $(function (){
         if ($(this).val() === "") {
             $(this).addClass("empty");
         }
+
+        if ($(this).parent().next().is("[type=file]")) {
+            $(this).parent().addClass("fileinput");
+            var $input = $(this).parent().next().detach();
+            $(this).after($input);
+        }
     });
 
     $(document).on("keyup change", ".form-control", function() {
@@ -31,7 +37,28 @@ $(function (){
             $(this).addClass("empty");
         }
     });
-    $(document).on("keydown", ".form-control", function() {
+    $(document).on("keydown change", ".form-control", function() {
         $(this).removeClass("empty");
+    });
+    $(document)
+    .on("focus", ".form-control-wrapper.fileinput", function() {
+        $(this).find("input").addClass("focus");
+    })
+    .on("blur", ".form-control-wrapper.fileinput", function() {
+        $(this).find("input").removeClass("focus");
+    })
+    .on("change", ".form-control-wrapper.fileinput [type=file]", function() {
+        var value = "";
+        $.each($(this)[0].files, function(i, file) {
+            console.log(file);
+            value += file.name + ", ";
+        });
+        value = value.substring(0, value.length - 2);
+        if (value) {
+            $(this).prev().removeClass("empty");
+        } else {
+            $(this).prev().addClass("empty");
+        }
+        $(this).prev().val(value);
     });
 });
