db3e1e9fcb6778e1a95d6a591ab866b2ca3ba8ce
Federico Zivolo <zivolof@p2000.it>
2014-09-26 13:29:07 +0200

    updated demo

--- material/material.js
+++ material/material.js
@@ -4,41 +4,55 @@ $(function (){
 
     ripples.init(".btn:not(.btn-link), .navbar a, .nav-tabs a, .withripple");
 
-    // Add fake-checkbox to material checkboxes
-    $(".checkbox > label > input").after("<span class=check></span>");
+    var initInputs = function() {
+        // Add fake-checkbox to material checkboxes
+        $(".checkbox > label > input").not(".bs-material").addClass("bs-material").after("<span class=check></span>");
 
-    // Add fake-radio to material radios
-    $(".radio > label > input").after("<span class=ripple></span><span class=circle></span><span class=check></span>");
+        // Add fake-radio to material radios
+        $(".radio > label > input").not(".bs-material").addClass("bs-material").after("<span class=circle></span><span class=check></span>");
 
-    // Add elements for material inputs
-    $("input.form-control, textarea.form-control, select.form-control").each( function() {
-        $(this).wrap("<div class=form-control-wrapper></div>");
-        $(this).after("<span class=material-input></span>");
-        if ($(this).hasClass("floating-label")) {
-            var placeholder = $(this).attr("placeholder");
-            $(this).attr("placeholder", null).removeClass("floating-label");
-            $(this).after("<div class=floating-label>" + placeholder + "</div>");
-        }
-        if ($(this).val() === "") {
-            $(this).addClass("empty");
-        }
+        // Add elements for material inputs
+        $("input.form-control, textarea.form-control, select.form-control").not(".bs-material").each( function() {
+            if ($(this).is(".bs-material")) { return; }
+            $(this).wrap("<div class=form-control-wrapper></div>");
+            $(this).after("<span class=material-input></span>");
+            if ($(this).hasClass("floating-label")) {
+                var placeholder = $(this).attr("placeholder");
+                $(this).attr("placeholder", null).removeClass("floating-label");
+                $(this).after("<div class=floating-label>" + placeholder + "</div>");
+            }
+            if ($(this).is(":empty") || $(this).val() === null || $(this).val() == "undefined" || $(this).val() === "") {
+                $(this).addClass("empty");
+            }
 
-        if ($(this).parent().next().is("[type=file]")) {
-            $(this).parent().addClass("fileinput");
-            var $input = $(this).parent().next().detach();
-            $(this).after($input);
-        }
-    });
+            if ($(this).parent().next().is("[type=file]")) {
+                $(this).parent().addClass("fileinput");
+                var $input = $(this).parent().next().detach();
+                $(this).after($input);
+            }
+        });
+
+    };
+    initInputs();
+
+    // Support for "arrive.js" to dynamically detect creation of elements
+    // include it before this script to take advantage of this feature
+    // https://github.com/uzairfarooq/arrive/
+    if (document.arrive) {
+        document.arrive("input, textarea, select", function() {
+            initInputs();
+        });
+    }
 
     $(document).on("keyup change", ".form-control", function() {
-        if ($(this).val() !== "") {
-            $(this).removeClass("empty");
-        } else {
-            $(this).addClass("empty");
-        }
-    });
-    $(document).on("keydown change", ".form-control", function() {
-        $(this).removeClass("empty");
+        var self = $(this);
+        setTimeout(function() {
+            if (self.val() === "") {
+                self.addClass("empty");
+            } else {
+                self.removeClass("empty");
+            }
+        }, 1);
     });
     $(document)
     .on("focus", ".form-control-wrapper.fileinput", function() {
