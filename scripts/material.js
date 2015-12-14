6ec039b5dfe90f46b1d5e55cfcb8593d03c4496f
Kevin Ross <kevin.ross@alienfast.com>
2015-12-13 19:28:39 -0600

    Ignore invalid key codes for isChar

--- scripts/material.js
+++ scripts/material.js
@@ -14,7 +14,16 @@
     if (typeof evt.which == "undefined") {
       return true;
     } else if (typeof evt.which == "number" && evt.which > 0) {
-      return !evt.ctrlKey && !evt.metaKey && !evt.altKey && evt.which != 8 && evt.which != 9;
+      return !evt.ctrlKey
+        && !evt.metaKey
+        && !evt.altKey
+        && evt.which != 8  // backspace
+        && evt.which != 9  // tab
+        && evt.which != 13 // enter
+        && evt.which != 16 // shift
+        && evt.which != 17 // ctrl
+        && evt.which != 20 // caps lock
+        && evt.which != 27; // escape
     }
     return false;
   }
