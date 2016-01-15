ae7a1ca27970779d1ec9960bf91d7e98fda36026
Kevin Ross <kevin.ross@alienfast.com>
2016-01-15 13:53:30 -0600

    #822 allow marker class mdb-drawer-closed to force closed the drawer i.e. for use with js (overrides responsive marker class)

--- docs/examples/dashboard/example.js
+++ docs/examples/dashboard/example.js
@@ -1,46 +1,45 @@
 function clearDrawerClasses($container) {
-  var classes = [ "mdb-drawer-fixed-left", "mdb-drawer-fixed-right", "mdb-drawer-fixed-top", "mdb-drawer-fixed-bottom" ];
+  var classes = ["mdb-drawer-fixed-left", "mdb-drawer-fixed-right", "mdb-drawer-fixed-top", "mdb-drawer-fixed-bottom"];
 
-  $.each(classes, function(index, value) {
+  $.each(classes, function (index, value) {
     $container.removeClass(value)
   })
 }
 
-function setDrawerPosition(position){
+function setDrawerPosition(position) {
   var $container = $('.mdb-layout-container')
 
   clearDrawerClasses($container)
   $container.addClass(position)
 }
 
-(function() { // document ready is a little convoluted because this executes before jquery.
-  setTimeout(function() {
+$(document).ready(function() { // document ready is a little convoluted because this executes before jquery.
+  var buttons = ["drawer-fixed-left", "drawer-fixed-right", "drawer-fixed-top", "drawer-fixed-bottom"]
 
-    var buttons = ["drawer-fixed-left", "drawer-fixed-right", "drawer-fixed-top", "drawer-fixed-bottom"]
-
-    $.each(buttons, function(index, position) {
-      $('#'+position).click(function(){
-        setDrawerPosition('mdb-' + position)
-      })
+  $.each(buttons, function (index, position) {
+    $('#' + position).click(function() {
+      setDrawerPosition('mdb-' + position)
     })
+  })
 
-    // add a toggle for drawer visibility that shows anytime
-    $('#drawer-visibility').click(function(){
-      var $container = $('.mdb-layout-container')
+  // add a toggle for drawer visibility that shows anytime
+  $('#drawer-visibility').click(function () {
+    var $container = $('.mdb-layout-container')
 
-      // once clicked, just do away with responsive marker
-      //$container.removeClass('mdb-drawer-open-md')
+    // once clicked, just do away with responsive marker
+    //$container.removeClass('mdb-drawer-open-md')
 
-      var $icon = $(this).find('.material-icons')
-      if($icon.text() == 'visibility_off'){
-        $container.removeClass('mdb-drawer-open')
-        $icon.text('visibility')
-      }
-      else {
-        $container.addClass('mdb-drawer-open')
-        $icon.text('visibility_off')
-      }
-    })
+    var $icon = $(this).find('.material-icons')
+    if ($icon.text() == 'visibility_off') {
+      $container.addClass('mdb-drawer-closed')
+      $container.removeClass('mdb-drawer-open') // demo only, regardless of the responsive class, we want to force it close
+      $icon.text('visibility')
+    }
+    else {
+      $container.removeClass('mdb-drawer-closed')
+      $container.addClass('mdb-drawer-open') // demo only, regardless of the responsive class, we want to force it open
+      $icon.text('visibility_off')
+    }
+  })
+})
 
-  }, 100)
-})();
---
+++ docs/examples/signin/example.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $('#inputEmail').focus()
+})
+
