1bd13ac67152b33f974ce32402100a0f651c3091
FezVrasta <federico.zivolo@gmail.com>
2016-08-05 14:47:59 +0200

    Revert "import BS sources"
    This reverts commit 13ce4bde672719216dd95ef974327c471e0fe90c.

--- js/index.js
+++ js/index.js
@@ -11,17 +11,7 @@
 import 'babel-polyfill'
 
 // Bootstrap components
-import 'bootstrap/js/src/alert'
-import 'bootstrap/js/src/button'
-import 'bootstrap/js/src/carousel'
-import 'bootstrap/js/src/collapse'
-import 'bootstrap/js/src/dropdown'
-import 'bootstrap/js/src/modal'
-import 'bootstrap/js/src/popover'
-import 'bootstrap/js/src/scrollspy'
-import 'bootstrap/js/src/tab'
-import 'bootstrap/js/src/tooltip'
-import 'bootstrap/js/src/util'
+import 'bootstrap/dist/js/bootstrap'
 
 // invalidComponentMatches is currently disabled due to https://github.com/rollup/rollup/issues/428#issuecomment-170066452
 import './checkbox'
